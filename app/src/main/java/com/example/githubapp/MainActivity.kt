9f1f2f1c9954ca67991f0d085ed6db95b5b5e0cd
Azizxon8288 <azizxonweb@gmail.com>
2021-11-06 15:58:07 +0500

    add function Toaster.kt class add button MainActivity.kt

--- app/src/main/java/com/example/githubapp/MainActivity.kt
+++ app/src/main/java/com/example/githubapp/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.githubapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.example.githubapp.databinding.ActivityMainBinding
+import com.example.toasterlibrary.getDistance
 import com.example.toasterlibrary.toaster
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +19,10 @@ class MainActivity : AppCompatActivity() {
 
         binding.btn.setOnClickListener {
             toaster(this, "Salomlar")
+        }
+        binding.btn2.setOnClickListener {
+            val distance = getDistance(10.0, 15.0, 90.0, 60.0)
+            toaster(this, distance)
         }
     }
 }
--- toasterLibrary/src/main/java/com/example/toasterlibrary/Toaster.kt
+++ toasterLibrary/src/main/java/com/example/toasterlibrary/Toaster.kt
@@ -1,8 +1,15 @@
 package com.example.toasterlibrary
 
 import android.content.Context
+import android.location.Location
 import android.widget.Toast
 
 fun toaster(context: Context, str: Any) {
     Toast.makeText(context, "$str", Toast.LENGTH_SHORT).show()
+}
+
+fun getDistance(startLat: Double, startLng: Double, endLat: Double, endLng: Double): Float {
+    val result = FloatArray(1)
+    Location.distanceBetween(startLat, startLng, endLat, endLng, result)
+    return result[0] / 1000
 }
